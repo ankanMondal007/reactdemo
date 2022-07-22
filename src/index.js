9d1a8da07dd5ddd7dd876ba6a2e7356379381c0d
ankanMondal007 <mailtoankan2001@gmail.comn>
2022-07-22 22:07:02 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>HelloWorld</h1>
   </React.StrictMode>
 );
 
